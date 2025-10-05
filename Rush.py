b4d61a3b1d369f09adc050d9d0b9ff9666800a32
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2025-10-05 22:49:09 +0100

    bfs

--- Rush.py
+++ Rush.py
@@ -1,130 +1,112 @@
 import csv
 import copy
+from collections import deque
+
+
 
 
 class RushHourPuzzle: 
-  def __init__(self, board_height, board_widht,vehicles,walls,board):
-    self.board_height=board_height
-    self.board_widht=board_widht
-    self.vehicles=vehicles
-    self.walls=walls
-    self.board=[]
+    def __init__(self, board_height, board_width, vehicles, walls, board):
+        self.board_height = board_height
+        self.board_width = board_width
+        self.vehicles = vehicles
+        self.walls = walls
+        self.board = []
 
-  def setVehicles(self,csv_file):
+    def setVehicles(self, csv_file):
+        self.vehicles = []
+        self.walls = []
+        with open(csv_file, newline='') as csvfile:
+            read = csv.reader(csvfile)
+            ligne1 = next(read)
+            self.board_height, self.board_width = int(ligne1[0]), int(ligne1[1])
 
-      self.vehicles=[]
-      self.walls=[]
-      with open(csv_file, newline='') as csvfile:
-        read=csv.reader(csvfile)
-        ligne1=next(read)
-        self.board_height,self.board_widht=int(ligne1[0]),int(ligne1[1])
+            for i in read:
+                if i[0] == '#':
+                    self.walls.append((int(i[1]), int(i[2])))
+                else:
+                    vehicle = {
+                        "id": i[0],
+                        "x": int(i[1]),
+                        "y": int(i[2]),
+                        "orientation": i[3],
+                        "length": int(i[4])
+                    }
+                    self.vehicles.append(vehicle)
+        self.setBoard()
 
-        for i in read:
-          if i[0]=='#':
-            self.walls.append((int(i[1]),int(i[2])))
-          else:
-            vehicle={
-              "id":i[0],
-              "x":int(i[1]),
-              "y":int(i[2]),
-              "orientation": i[3],
-              "length":int(i[4])
-            }
-            self.vehicles.append(vehicle)
+    def setBoard(self):
+        board = [[' ' for _ in range(self.board_width)] for _ in range(self.board_height)]
+        self.board = board
 
-      self.setBoard()
+        for x, y in self.walls:
+            self.board[y][x] = '#'
 
+        for v in self.vehicles:
+            vid = v["id"]
+            x = v["x"]
+            y = v["y"]
+            orientation = v["orientation"]
+            length = v["length"]
+            for i in range(length):
+                if orientation == "H":
+                    self.board[y][x + i] = vid
+                else:
+                    self.board[y + i][x] = vid
 
-def setBoard(self):
-    board = []
-    for y in range(self.board_height):
-        ligne = []
-        for x in range(self.board_width):
-            ligne.append(' ')
-        board.append(ligne)
-    self.board = board
-
-    for x, y in self.walls:
-        self.board[y][x] = '#'
-
-    for v in self.vehicles:
-        vid = v["id"]
-        x = v["x"]
-        y = v["y"]
-        orientation = v["orientation"]
-        length = v["length"]
-
-        for i in range(length):
-            if orientation == "H":
-                self.board[y][x + i] = vid
-            else:
-                self.board[y + i][x] = vid
-
-def isGoal(self):
-    for v in self.vehicles:
-        if v["id"] == "X":
-            if v["orientation"] == "H":
+    def isGoal(self):
+        for v in self.vehicles:
+            if v["id"] == "X" and v["orientation"] == "H":
                 last_x = v["x"] + v["length"] - 1
                 if last_x == self.board_width - 1:
                     return True
-    return False
+        return False
 
+    def successorFunction(self):
+        successors = []
+        for v in self.vehicles:
+            vid = v["id"]
+            x, y = v["x"], v["y"]
+            orientation, length = v["orientation"], v["length"]
 
+            if orientation == "H":
+                if x + length < self.board_width and self.board[y][x + length] == ' ':
+                    new_state = self._moveVehicle(v, dx=1, dy=0)
+                    successors.append(((vid, "forward"), new_state))
+                if x - 1 >= 0 and self.board[y][x - 1] == ' ':
+                    new_state = self._moveVehicle(v, dx=-1, dy=0)
+                    successors.append(((vid, "backward"), new_state))
 
+            elif orientation == "V":
+                if y + length < self.board_height and self.board[y + length][x] == ' ':
+                    new_state = self._moveVehicle(v, dx=0, dy=1)
+                    successors.append(((vid, "forward"), new_state))
+                if y - 1 >= 0 and self.board[y - 1][x] == ' ':
+                    new_state = self._moveVehicle(v, dx=0, dy=-1)
+                    successors.append(((vid, "backward"), new_state))
 
-def successorFunction(self):
-    successors = []
+        return successors
 
-    for v in self.vehicles:
-        vid = v["id"]
-        x = v["x"]
-        y = v["y"]
-        orientation = v["orientation"]
-        length = v["length"]
+    def _moveVehicle(self, vehicle, dx, dy):
+        new_puzzle = RushHourPuzzle(
+            board_height=self.board_height,
+            board_width=self.board_width,
+            vehicles=copy.deepcopy(self.vehicles),
+            walls=copy.deepcopy(self.walls),
+            board=[]
+        )
+        for v in new_puzzle.vehicles:
+            if v["id"] == vehicle["id"]:
+                v["x"] += dx
+                v["y"] += dy
+                break
+        new_puzzle.setBoard()
+        return new_puzzle
 
-        if orientation == "H":
-            #  Déplacement vers la droite 
-            if x + length < self.board_width and self.board[y][x + length] == ' ':
-                new_state = self._moveVehicle(v, dx=1, dy=0)
-                successors.append(((vid, "forward"), new_state))
-
-            # Déplacement vers la gauche 
-            if x - 1 >= 0 and self.board[y][x - 1] == ' ':
-                new_state = self._moveVehicle(v, dx=-1, dy=0)
-                successors.append(((vid, "backward"), new_state))
-
-        elif orientation == "V":
-            # Déplacement vers le bas
-            if y + length < self.board_height and self.board[y + length][x] == ' ':
-                new_state = self._moveVehicle(v, dx=0, dy=1)
-                successors.append(((vid, "forward"), new_state))
-
-            # Déplacement vers le haut
-            if y - 1 >= 0 and self.board[y - 1][x] == ' ':
-                new_state = self._moveVehicle(v, dx=0, dy=-1)
-                successors.append(((vid, "backward"), new_state))
-
-    return successors
-
-
-def _moveVehicle(self, vehicle, dx, dy):
-    new_puzzle = RushHourPuzzle(
-        board_height=self.board_height,
-        board_width=self.board_width,
-        vehicles=copy.deepcopy(self.vehicles),
-        walls=copy.deepcopy(self.walls)
-    )
-
-    # Déplacer le véhicule correspondant
-    for v in new_puzzle.vehicles:
-        if v["id"] == vehicle["id"]:
-            v["x"] += dx
-            v["y"] += dy
-            break
-
-    new_puzzle.setBoard()
-    return new_puzzle
-
+    def showBoard(self):
+        for row in self.board:
+            print(" ".join(row))
+        print("-" * (2 * self.board_width - 1))
 
 class Node:
     def __init__(self,state,parent=None,action=None, g=0,f=0):
@@ -159,3 +141,60 @@ class Node:
         
 
 
+
+def bfs(initial_state):
+    root = Node(state=initial_state, parent=None, action=None, g=0)
+
+    if initial_state.isGoal():
+        return root
+
+    queue = deque([root])
+    visited = [initial_state.board]  
+
+    while queue:
+        node = queue.popleft()
+
+        # Successeurs
+        for action, new_state in node.state.successorFunction():
+            
+            is_new = True
+            for b in visited:
+                if new_state.board == b: 
+                    is_new = False
+                    break
+
+            if is_new:
+                child = Node(state=new_state, parent=node, action=action, g=node.g + 1)
+
+                if new_state.isGoal():
+                    print("✅ Goal found!")
+                    return child
+
+                queue.append(child)
+                visited.append(copy.deepcopy(new_state.board))  
+
+    print(" No solution found.")
+    return None
+
+
+
+if __name__ == "__main__":
+    place = RushHourPuzzle(0, 0, [], [], [])
+    place.setVehicles("1.csv")
+
+    print("Plateau initial :")
+    place.showBoard()
+
+    
+    print("\n solution avec BFS...")
+    solution_node = bfs(place)
+
+    if solution_node:
+        print("\n Solution trouvée en", solution_node.g, "mouvements :")
+        print(" Séquence d’actions :", solution_node.getSolution())
+
+        print("\n États du chemin :")
+        for state in solution_node.getPath():
+            state.showBoard()
+    else:
+        print(" Aucune solution trouvée.")
